fd64ee1ce7c1868979b2aeaca30fe18fbb5756cf
SebastianCasanova4 <s.casanovao@uniandes.edu.co>
2022-10-24 03:30:59 -0500

    Taller TypeScript

---
+++ scripts/Serie.js
@@ -0,0 +1,13 @@
+var Serie = /** @class */ (function () {
+    function Serie(id, name, canal, temporadas, descripcion, enlace, imagen) {
+        this.id = id;
+        this.name = name;
+        this.canal = canal;
+        this.temporadas = temporadas;
+        this.descripcion = descripcion;
+        this.enlace = enlace;
+        this.imagen = imagen;
+    }
+    return Serie;
+}());
+export { Serie };
