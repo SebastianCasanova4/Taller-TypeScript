024e07becd5c665f11fb7caaa73fddeb00d2c584
SebastianCasanova4 <s.casanovao@uniandes.edu.co>
2022-11-07 11:12:44 -0500

    Pequeños cambios

--- Serie.ts
+++ Serie.ts
@@ -1,5 +1,4 @@
 export class Serie{
     constructor(public id: number, public name: string, public canal: string, public temporadas: number, public descripcion: string, public enlace: string, public imagen: string){
-
     }
 }
